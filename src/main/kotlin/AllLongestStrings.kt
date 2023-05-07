5b65e719b60332b455486ce0e6d7d99dbb723e16
Marcos Poggi <kikopoggi@gmail.com>
2023-05-07 09:57:22 -0300

    AllLongestStrings adjust

--- src/main/kotlin/AllLongestStrings.kt
+++ src/main/kotlin/AllLongestStrings.kt
@@ -1,20 +1,20 @@
 fun main() {
-    val stringsArray = mutableListOf<String>("aba", "aa", "ad", "vcd", "aba")
+    val stringsArray = mutableListOf("aba", "aa", "ad", "vcd", "aba")
     solutionALS(stringsArray)
 }
 
 fun solutionALS(inputArray: MutableList<String>): MutableList<String> {
     val outputArray = mutableListOf<String>()
-    var maxLenght = 0
+    var maxLength = 0
 
     for (word in inputArray) {
-        if (word.length > maxLenght) {
-            maxLenght = word.length
+        if (word.length > maxLength) {
+            maxLength = word.length
         }
     }
 
     inputArray.forEach { word ->
-        if (word.length == maxLenght) {
+        if (word.length == maxLength) {
             outputArray.add(word)
         }
     }
